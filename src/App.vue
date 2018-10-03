ea894978ed1c2e2c8acc4510b5f00f51f5db9ea6
Iury Almeida <iurygh@gmail.com>
2018-10-03 18:55:04 -0300

    Estruturação Vue

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+
+	<div>
+
+		<h2>{{ titulo }}</h2>
+
+		<ul>
+			<li v-for="foto of fotos">
+				<img :src="foto.url" :alt="foto.titulo">
+			</li>
+		</ul>
+	</div>
+
+</template>
+
+<script>
+export default {
+
+	data(){
+		return {
+			titulo: 'Olá Paz uzu',
+			fotos: [
+				{
+					url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/PazuzuDemonAssyria1stMillenniumBCE.jpg/200px-PazuzuDemonAssyria1stMillenniumBCE.jpg',
+					titulo: 'Pazuzu'
+				},
+				{
+					url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/PazuzuDemonAssyria1stMillenniumBCE.jpg/200px-PazuzuDemonAssyria1stMillenniumBCE.jpg',
+					titulo: 'Pazuzuss'
+				}	
+			]
+		}
+	}
+}
+</script>
+	
+<style lang="scss">
+
+</style>
